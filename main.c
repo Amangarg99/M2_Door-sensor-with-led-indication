faba958150e5070a22c172623dec670e30a4cce1
Aman Garg <gargaman335@gmail.com>
2022-04-26 11:35:54 +0530

    Changes in main file

--- main.c
+++ main.c
@@ -7,16 +7,10 @@ void change_switch_state(uint8_t state)
 }
 int main(void)
 {
-
 	DDRB=DDRB&0b11111101; 
 	DDRC=DDRC|0b01000000; 
 	while(1)
-	{
-		 change_switch_state(switch_ON);
-		delay_ms(switch_ON_TIME);
-		
-        change_led_state(switch_OFF);
-		delay_ms(switch_OFF_TIME); 	
+	{	
 if(PINB & 0b00000010) 
 	PORTC=PORTC|0b01000000; 
 		else
